5a6f485b0c8c2ca175e5c0cc31ef8ada1ba4f5a6
fiadh <fiadh@byu.edu>
2024-11-13 18:11:33 -0700

    Create the router

--- src/app.jsx
+++ src/app.jsx
@@ -2,44 +2,52 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Gameplay } from './gameplay/gameplay';
+import { Scores } from './scores/scores';
+
 export default function App() {
     return (
-      <div className='body'>
-        <header className='container-fluid'>
-          <nav className='navbar fixed-top'>
-            <div className='navbar-brand'>
-              Palette Path
-            </div>
-            <menu className='navbar-nav'>
-              <li className='nav-item'>
-                <a className='nav-link' href='index.html'>
-                  Home
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='scores.html'>
-                  Scores
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='gameplay.html'>
-                  Gameplay
-                </a>
-              </li>
-            </menu>
-          </nav>
-        </header>
-  
-        <main>App components go here</main>
-  
-        <footer className='footer'>
-          <div className='container-fluid'>
-            <span className='text-reset'>Heidi Hart</span>
-            <a className='text-reset' href='https://github.com/fiadhhart/startup'>
-              Github
-            </a>
-          </div>
-        </footer>
-      </div>
+      <BrowserRouter>
+        <div className='body'>
+            <header className='container-fluid'>
+                <nav className='navbar fixed-top'>
+                    <div className='navbar-brand'>Palette Path</div>
+                    <menu className='navbar-nav'>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to=''>Home</NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='scores'>Scores</NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='gameplay'>Gameplay</NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
+    
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/gameplay' element={<Gameplay />} />
+                <Route path='/scores' element={<Scores />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+    
+            <footer className='footer'>
+                <div className='container-fluid'>
+                    <span className='text-reset'>Heidi Hart</span>
+                    <a className='text-reset' href='https://github.com/fiadhhart/startup'>Github</a>
+                </div>
+            </footer>
+        </div>
+      </BrowserRouter>
     );
   }
+
+  function NotFound() {
+    return <main>404: Return to sender. Address unknown.</main>;
+  }
+
+  
---
+++ src/gameplay/gameplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Gameplay() {
+  return (
+    <main>
+      <div>gameplay displayed here</div>
+    </main>
+  );
+}
