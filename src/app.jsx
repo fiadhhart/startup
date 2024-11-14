3722bb702fc366a46c59321c35f0cd278c11fc45
fiadh <fiadh@byu.edu>
2024-11-13 17:35:01 -0700

    app.jsx 2

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+      <div className='body'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top'>
+            <div className='navbar-brand'>
+              Palette Path
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link' href='index.html'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='scores.html'>
+                  Scores
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='gameplay.html'>
+                  Gameplay
+                </a>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <main>App components go here</main>
+  
+        <footer className='footer'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Heidi Hart</span>
+            <a className='text-reset' href='https://github.com/fiadhhart/startup'>
+              Github
+            </a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
